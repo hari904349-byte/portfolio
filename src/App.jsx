f8fca07fefb5ff3113e9e90e95a8cc11838f548f
hari904349-byte <hari904349@gmail.com>
2026-05-29 13:23:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import './App.css'
 
 function App() {
